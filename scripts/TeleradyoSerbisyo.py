ef3c0e89037cc7c30e6b3355d6ad465ffc0fc737
Feitan <21117583+ex3mpli@users.noreply.github.com>
2024-08-02 16:57:31 +0800

    Update and rename cctvhitdrama.py to TeleradyoSerbisyo.py

--- scripts/TeleradyoSerbisyo.py
+++ scripts/TeleradyoSerbisyo.py
@@ -37,7 +37,7 @@ def grab(url):
 print("#EXTM3U")
 print("#EXT-X-STREAM-INF:BANDWIDTH=10000000")
 s = requests.Session()
-with open("../information/cctvhitdrama.txt") as f:
+with open("../information/TeleradyoSerbisyo.txt") as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith("~~"):
