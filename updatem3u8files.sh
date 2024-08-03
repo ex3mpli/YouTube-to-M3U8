3426faae12a1b835527c5c94250d892343cb106e
Feitan <21117583+ex3mpli@users.noreply.github.com>
2024-08-03 20:12:00 +0800

    Update updatem3u8files.sh

--- updatem3u8files.sh
+++ updatem3u8files.sh
@@ -3,4 +3,6 @@ echo $(dirname $0)
 python3 -m pip install requests
 cd $(dirname $0)/scripts/
 python3 abscbn.py > ../streams/abscbn.m3u8
+python3 gmanetwork.py > ../streams/gmanetwork.m3u8
+python3 gmanews.py > ../streams/gmanews.m3u8
 echo m3u8 grabbed
